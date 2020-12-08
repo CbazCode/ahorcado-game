bb381a1b61930dce61532e8594ea7f60368ff136
Sebastian Asis <sasisromero10@gmail.com>
2020-12-08 17:48:51 -0500

    function generarPalabra inserted

--- app.js
+++ app.js
@@ -1,37 +1,40 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap4 -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles.css">
-    <title>Ahorcado</title>
-</head>
-<body>
-    <header>
-        <nav>
-            <div class = "parrafo-nav">
-                <p>Ahorcados</p>
-            </div>
-        </nav>
-    </header>
-    <div class="contenedor-juego">
-        <div class="ahorcado-img"></div>
-        <div class="contenedor-input">
-            <div class = "contenedor-principal">
-                <div class="contenedor-palabra"></div>
-            </div>
-            <form action="" class="form">
-                <label for="input-letra">Ingrese una letra: </label>
-                <input name="letra" id="input-letra" type="text" autocomplete="off">
-                <button type="submit" class="btn-submit btn btn-primary">Ingresar</button>
-                <button type="button" class = "reinicio btn btn-warning " style="display:none;">Reiniciar</button>
-            </form>
-        </div>
-    </div>
-    <script src="app.js"></script>
-    <!-- Sweet-aler2 -->
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-</body>
-</html>
+const form = document.querySelector('.form');
+const valueiInput = document.querySelector('#input-letra');
+
+let divAhorcado = document.querySelector('.ahorcado-img');
+
+const vector = ["MARIPOSA" , "GOKU", "CHIKISTRIKIS","ASISEHACE",
+                "PAPAGAYO", "CELULAR", "CARGADOR","TRIPODE",
+                "EEG","UNIVERSIDAD", "CODIGO", "JAVASCRIPT",
+                "CBAZCODE"];
+
+//Variables globales
+let palabraEnJuego;
+let letra;
+let comenzamos;
+let indiceRandom;
+
+//Vector
+let vectorLetrasEncontradas = [];
+
+
+//Banderas
+let estaJugado = false;
+let encontrado =false;
+let primeraVez = true;
+
+//Contadores
+let contadorFallos = 0;
+let contadorLetrasMostradas = 0;
+
+
+
+
+
+const generarPalabra = () =>{
+      indiceRandom = obtenerNumeroRandom(0,(vector.length-1));
+      console.log(indiceRandom);
+      palabraEnJuego = vector[indiceRandom];
+      estaJugado = true;
+      creadorLetras(palabraEnJuego);
+}
